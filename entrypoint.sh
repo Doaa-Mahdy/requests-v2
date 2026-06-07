abcab2ceb07b83eb79ad4a8138677ddf0ec9064d
Doaa-Mahdy <D.Mahdy2224@nu.edu.eg>
2026-06-07 08:30:51 +0300

    edit enterypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,3 @@ done
 echo "Pulling model..."
 ollama pull qwen2.5:7b
 
-# echo "Downloading HF models (runtime safe)..."
-# python3 app/scripts/download_models.py
-
-echo "Starting app..."
-exec python3 main.py
